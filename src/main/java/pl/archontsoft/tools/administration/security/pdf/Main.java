e67632798f112430dc4bc95cbebe92269946261e
Daniel Michalski <danielp.michalski@gmail.com>
2024-09-09 10:52:52 +0200

    improved naming and protection settings

--- src/main/java/pl/archontsoft/tools/administration/security/pdf/Main.java
+++ src/main/java/pl/archontsoft/tools/administration/security/pdf/Main.java
@@ -9,42 +9,51 @@ import org.apache.pdfbox.pdmodel.encryption.StandardProtectionPolicy;
 public class Main {
 
   public static void main(String[] args) throws IOException {
+    // todo extract 2 functions - 1 for fully unprotecting the pdf, 1 for protecting the pdf
+    // todo consider running a simple webapp to allow others to do the same for free - must be very secure!
+
     // absolute path
     String path = "";
     // fileName without extension
     String fileName = "";
     String ext = ".pdf";
 
-    // fill ownerPassword to make the document non-editable unless this pwd is entered
-    String ownerPassword = "";
-
-    PDDocument doc = PDDocument.load(new File(path + File.separator + fileName + ext));
+    PDDocument document = PDDocument.load(
+      new File(path + File.separator + fileName + ext),
+      "" // password
+    );
 
     // Define the length of the encryption key.
     // Possible values are 40, 128 or 256.
-    int keyLength = 256;
 
-    AccessPermission ap = new AccessPermission();
+    AccessPermission accessPermission = new AccessPermission();
 
     // printing
-    ap.setCanPrint(true);
-    ap.setCanPrintDegraded(true);
+    accessPermission.setCanPrint(false);
+    accessPermission.setCanPrintDegraded(false);
     // copying
-    ap.setCanExtractContent(true);
+    accessPermission.setCanExtractContent(false);
     // Disable other things if needed...
-    ap.setCanModify(false);
-    ap.setCanFillInForm(false);
-    ap.setCanModifyAnnotations(false);
+    accessPermission.setCanModify(false);
+    accessPermission.setCanFillInForm(false);
+    accessPermission.setCanModifyAnnotations(false);
 
     // Owner password (to open the file with all permissions) is "12345"
+    String ownerPassword = "to jest bardzo dlugie haslo malo kto je zlamie";
     // User password (to open the file but with restricted permissions, is empty here)
-    StandardProtectionPolicy spp = new StandardProtectionPolicy(ownerPassword, "", ap);
-    spp.setEncryptionKeyLength(keyLength);
+    String userPassword = "";
+
+    StandardProtectionPolicy protectionPolicy = new StandardProtectionPolicy(ownerPassword, userPassword, accessPermission);
+    protectionPolicy.setEncryptionKeyLength(256);
 
     // Apply protection
-    doc.protect(spp);
+//    document.protect(protectionPolicy);
 
-    doc.save(new File(path + File.separator + fileName + "-protected" + ext));
-    doc.close();
+    // remove all security
+    document.setAllSecurityToBeRemoved(true);
+
+//    document.save(new File(path + File.separator + fileName + "-protected" + ext));
+    document.save(new File(path + File.separator + fileName + "-unprotected" + ext));
+    document.close();
   }
 }
